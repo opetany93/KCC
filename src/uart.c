367e3d477eee30bebc39c0a72cfae7af6a6ffd03
tainfante <agatadul@gmail.com>
2018-01-20 16:07:23 +0100

    sending data: long time

--- src/uart.c
+++ src/uart.c
@@ -33,3 +33,19 @@ void sendData(uint8_t* data, uint16_t size)
 		sendByte((char)*data++);
 	}
 }
+void sendOneByte(uint8_t data)
+{
+	sendByte((char) data);
+
+}
+void sendLong(unsigned long long time){
+	time=time|0x0000010000000000;
+
+	sendOneByte((uint8_t)((time >> 40)&0xFF));
+	sendOneByte((uint8_t)((time >> 32)&0xFF));
+	sendOneByte((uint8_t)((time >> 24)&0xFF));
+	sendOneByte((uint8_t)((time >> 16)&0xFF));
+	sendOneByte((uint8_t)((time >> 8)&0xFF));
+	sendOneByte((uint8_t)(time&0xFF));
+
+}
